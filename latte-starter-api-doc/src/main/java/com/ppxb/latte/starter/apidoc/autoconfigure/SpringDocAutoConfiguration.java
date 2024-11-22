e7136233efd3490a5b6ac082e131891ce8965d11
ppxb <317842449@qq.com>
2024-11-22 11:59:24 +0800

    doc: add SpringDocAutoConfiguration method javadoc

--- latte-starter-api-doc/src/main/java/com/ppxb/latte/starter/apidoc/autoconfigure/SpringDocAutoConfiguration.java
+++ latte-starter-api-doc/src/main/java/com/ppxb/latte/starter/apidoc/autoconfigure/SpringDocAutoConfiguration.java
@@ -23,7 +23,6 @@
  */
 
 
-
 package com.ppxb.latte.starter.apidoc.autoconfigure;
 
 import cn.hutool.core.map.MapUtil;
@@ -105,8 +104,8 @@ public class SpringDocAutoConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/")
-            .setCacheControl(CacheControl.maxAge(5, TimeUnit.HOURS).cachePublic());
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+                .setCacheControl(CacheControl.maxAge(5, TimeUnit.HOURS).cachePublic());
     }
 
     /**
@@ -132,26 +131,39 @@ public class SpringDocAutoConfiguration implements WebMvcConfigurer {
         openAPI.info(buildInfo(projectProperties));
 
         Optional.ofNullable(springDocExtensionProperties.getComponents())
-            .ifPresent(components -> configureComponents(openAPI, components));
+                .ifPresent(components -> configureComponents(openAPI, components));
         return openAPI;
     }
 
+
+    /**
+     * 创建OpenAPI Info实例。
+     *
+     * @param projectProperties 项目配置属性
+     * @return 配置好的OpenAPI Info实例
+     */
     private Info buildInfo(ProjectProperties projectProperties) {
         Info info = new Info().title(String.format("%s %s", projectProperties.getName(), "API 文档"))
-            .version(projectProperties.getVersion())
-            .description(projectProperties.getDescription());
+                .version(projectProperties.getVersion())
+                .description(projectProperties.getDescription());
 
         Optional.ofNullable(projectProperties.getContact())
-            .ifPresent(contact -> info.contact(new Contact().name(contact.getName())
-                .email(contact.getEmail())
-                .url(contact.getUrl())));
+                .ifPresent(contact -> info.contact(new Contact().name(contact.getName())
+                        .email(contact.getEmail())
+                        .url(contact.getUrl())));
 
         Optional.ofNullable(projectProperties.getLicense())
-            .ifPresent(license -> info.license(new License().name(license.getName()).url(license.getUrl())));
+                .ifPresent(license -> info.license(new License().name(license.getName()).url(license.getUrl())));
 
         return info;
     }
 
+    /**
+     * 注册安全组件。
+     *
+     * @param openAPI    openAPI实例
+     * @param components 组件
+     */
     private void configureComponents(OpenAPI openAPI, Components components) {
         openAPI.components(components);
 
@@ -175,8 +187,8 @@ public class SpringDocAutoConfiguration implements WebMvcConfigurer {
     @ConditionalOnMissingBean
     public GlobalOpenApiCustomizer globalOpenApiCustomizer(SpringDocExtensionProperties springDocExtensionProperties) {
         return openApi -> Optional.ofNullable(openApi.getPaths())
-            .ifPresent(paths -> paths.forEach((key, item) -> configureSecurityForPath(item, springDocExtensionProperties
-                .getComponents())));
+                .ifPresent(paths -> paths.forEach((key, item) -> configureSecurityForPath(item, springDocExtensionProperties
+                        .getComponents())));
     }
 
     private void configureSecurityForPath(PathItem pathItem, Components components) {
--- latte-starter-api-doc/src/main/java/com/ppxb/latte/starter/apidoc/handler/OpenAPIHandler.java
+++ latte-starter-api-doc/src/main/java/com/ppxb/latte/starter/apidoc/handler/OpenAPIHandler.java
@@ -57,35 +57,92 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * OpenAPI文档处理器，负责构建和管理API文档的标签、安全配置等信息。
+ * 该类继承自{@link OpenAPIService}，提供了更丰富的文档处理功能。
+ *
+ * <p>主要功能：
+ * <ul>
+ * <li>处理API文档的标签(Tags)生成</li>
+ * <li>管理安全配置要求</li>
+ * <li>集成JavaDoc文档</li>
+ * <li>支持自定义API构建</li>
+ * </ul>
+ *
+ * @author ppxb
+ * @since 1.0.0
+ */
 @SuppressWarnings("all")
 public class OpenAPIHandler extends OpenAPIService {
 
+    /**
+     * 基础错误控制器类
+     */
     private static Class<?> basicErrorController;
 
+    /**
+     * 安全服务，用于处理API安全相关配置
+     */
     private final SecurityService securityService;
 
+    /**
+     * 映射关系缓存
+     */
     private final Map<String, Object> mappingsMap = new HashMap<>();
 
+    /**
+     * SpringDoc标签缓存
+     */
     private final Map<HandlerMethod, Tag> springdocTags = new HashMap<>();
 
+    /**
+     * OpenAPI构建器自定义器列表
+     */
     private final Optional<List<OpenApiBuilderCustomizer>> openApiBuilderCustomizers;
 
+    /**
+     * 服务器基础URL自定义器列表
+     */
     private final Optional<List<ServerBaseUrlCustomizer>> serverBaseUrlCustomizers;
 
+    /**
+     * SpringDoc配置属性
+     */
     private final SpringDocConfigProperties springDocConfigProperties;
 
+    /**
+     * OpenAPI实例缓存
+     */
     private final Map<String, OpenAPI> cachedOpenAPI = new HashMap<>();
 
+    /**
+     * 属性解析工具
+     */
     private final PropertyResolverUtils propertyResolverUtils;
 
+    /**
+     * JavaDoc提供者
+     */
     private final Optional<JavadocProvider> javadocProvider;
 
+    /**
+     * Spring应用上下文
+     */
     private ApplicationContext context;
 
+    /**
+     * OpenAPI实例
+     */
     private OpenAPI openAPI;
 
+    /**
+     * 服务器配置是否存在标志
+     */
     private boolean isServersPresent;
 
+    /**
+     * 服务器基础URL
+     */
     private String serverBaseUrl;
 
     public OpenAPIHandler(Optional<OpenAPI> openAPI,
@@ -119,6 +176,15 @@ public class OpenAPIHandler extends OpenAPIService {
         }
     }
 
+    /**
+     * 将集合转换为Set集合的工具方法。
+     *
+     * @param collection 源集合
+     * @param function   转换函数
+     * @param <E>        源集合元素类型
+     * @param <T>        目标集合元素类型
+     * @return 转换后的Set集合
+     */
     public static <E, T> Set<T> toSet(Collection<E> collection, Function<E, T> function) {
         if (CollUtil.isEmpty(collection) || function == null) {
             return CollUtil.newHashSet();
@@ -126,6 +192,23 @@ public class OpenAPIHandler extends OpenAPIService {
         return collection.stream().map(function).filter(Objects::nonNull).collect(Collectors.toSet());
     }
 
+    /**
+     * 构建API操作的Tag信息。
+     *
+     * <p>该方法处理以下标签来源：
+     * <ul>
+     * <li>方法级别的Tag注解</li>
+     * <li>类级别的Tag注解</li>
+     * <li>JavaDoc文档</li>
+     * <li>自动生成的Tag</li>
+     * </ul>
+     *
+     * @param handlerMethod 处理器方法
+     * @param operation     API操作
+     * @param openAPI       OpenAPI实例
+     * @param locale        区域设置
+     * @return 更新后的API操作
+     */
     @Override
     public Operation buildTags(HandlerMethod handlerMethod, Operation operation, OpenAPI openAPI, Locale locale) {
         Set<Tag> tags = new HashSet<>();
@@ -195,6 +278,14 @@ public class OpenAPIHandler extends OpenAPIService {
         return operation;
     }
 
+    /**
+     * 从方法注解中构建Tag信息。
+     *
+     * @param method  方法对象
+     * @param tags    Tag集合
+     * @param tagsStr Tag字符串集合
+     * @param locale  区域设置
+     */
     private void buildTagsFromMethod(Method method, Set<Tag> tags, Set<String> tagsStr, Locale locale) {
         Set<Tags> tagsSet = AnnotatedElementUtils.findAllMergedAnnotations(method, Tags.class);
         Set<io.swagger.v3.oas.annotations.tags.Tag> methodTags = tagsSet.stream()
@@ -210,6 +301,20 @@ public class OpenAPIHandler extends OpenAPIService {
 
     }
 
+    /**
+     * 添加Tag到Tag集合中。
+     *
+     * <p>该方法会：
+     * <ul>
+     * <li>解析Tag名称和描述</li>
+     * <li>处理国际化</li>
+     * <li>确保Tag唯一性</li>
+     * </ul>
+     *
+     * @param sourceTags 源Tag列表
+     * @param tags       目标Tag集合
+     * @param locale     区域设置
+     */
     private void addTags(List<io.swagger.v3.oas.annotations.tags.Tag> sourceTags, Set<Tag> tags, Locale locale) {
         Optional<Set<Tag>> optionalTagSet = AnnotationsUtils.getTags(sourceTags
             .toArray(new io.swagger.v3.oas.annotations.tags.Tag[0]), true);
