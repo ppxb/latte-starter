cb695781bcb136f0532173995a787566836ba4a0
ppxb <317842449@qq.com>
2024-12-10 10:19:22 +0800

    chore: add mvc module

---
+++ latte-starter-web/src/main/java/com/ppxb/latte/starter/web/autoconfigure/mvc/WebMvcAutoConfiguration.java
@@ -0,0 +1,78 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 ppxb
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+
+package com.ppxb.latte.starter.web.autoconfigure.mvc;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+import java.util.Objects;
+
+@EnableWebMvc
+@AutoConfiguration
+public class WebMvcAutoConfiguration implements WebMvcConfigurer {
+
+    private static final Logger log = LoggerFactory.getLogger(WebMvcAutoConfiguration.class);
+
+    private final MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    public WebMvcAutoConfiguration(MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter) {
+        this.mappingJackson2HttpMessageConverter = mappingJackson2HttpMessageConverter;
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.removeIf(MappingJackson2HttpMessageConverter.class::isInstance);
+        if (Objects.isNull(mappingJackson2HttpMessageConverter)) {
+            converters.addFirst(new MappingJackson2HttpMessageConverter());
+        } else {
+            converters.addFirst(mappingJackson2HttpMessageConverter);
+        }
+        // 自定义 converters 时，需要手动在最前面添加 ByteArrayHttpMessageConverter
+        // 否则 Spring Doc OpenAPI 的 /*/api-docs/**（例如：/v3/api-docs/default）接口响应内容会变为 Base64 编码后的内容，最终导致接口文档解析失败
+        // 详情请参阅：https://github.com/springdoc/springdoc-openapi/issues/2143
+        converters.addFirst(new ByteArrayHttpMessageConverter());
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverterFactory(new BaseEnumConverterFactory());
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        log.debug("[Latte Starter] - Auto Configuration 'Web MVC' completed initialization.");
+    }
+}
