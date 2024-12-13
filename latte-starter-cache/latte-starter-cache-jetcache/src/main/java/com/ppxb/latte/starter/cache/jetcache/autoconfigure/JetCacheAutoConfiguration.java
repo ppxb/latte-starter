382006b4261046f5c8c933f54aff46a89c35e0d6
ppxb <317842449@qq.com>
2024-12-13 08:54:30 +0800

    chore: format jetcache autoconfiguration

--- latte-starter-cache/latte-starter-cache-jetcache/src/main/java/com/ppxb/latte/starter/cache/jetcache/autoconfigure/JetCacheAutoConfiguration.java
+++ latte-starter-cache/latte-starter-cache-jetcache/src/main/java/com/ppxb/latte/starter/cache/jetcache/autoconfigure/JetCacheAutoConfiguration.java
@@ -27,12 +27,28 @@
 package com.ppxb.latte.starter.cache.jetcache.autoconfigure;
 
 import com.ppxb.latte.starter.core.util.GeneralPropertySourceFactory;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * JetCache 自动配置
+ *
+ * @author ppxb
+ * @since 1.0.0
+ */
 @AutoConfiguration
 @Import(com.alicp.jetcache.autoconfigure.JetCacheAutoConfiguration.class)
 @PropertySource(value = "classpath:default-cache-jetcache.yml", factory = GeneralPropertySourceFactory.class)
 public class JetCacheAutoConfiguration {
+
+    private static final Logger log = LoggerFactory.getLogger(JetCacheAutoConfiguration.class);
+
+    @PostConstruct
+    public void postConstruct() {
+        log.debug("[Latte Starter] - Auto Configuration 'JetCache' completed initialization.");
+    }
 }
